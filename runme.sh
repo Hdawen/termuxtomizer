87cf49195e560ffacc5d77eb35087d02fa80f8ea
mechtifs <mechtifs@gmail.com>
2019-08-04 12:52:40 +0800

    v0.6

--- runme.sh
+++ runme.sh
@@ -1,10 +1,11 @@
+#!/bin/bash
 mv ~/.termux ~/.termuxbak
 cp -r .termux ~
 cp .hushlogin ~
 pkg install zsh wget neofetch
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 chsh -s zsh
-git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
