2a86685a7a3470dbe20c5d5ae0409a3958688616
mechtifs <mechtifs@gmail.com>
2019-08-19 03:18:59 +0800

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 while getopts ":r" opt; do
-    case $opt in
-        r ) rm -rf ~/.oh-my-zsh ~/.hushlogin ~/.zshrc ~/.termux; pkg remove zsh neofetch; exit;;
-        \? ) echo "Usage: runme.sh [-r]"; exit;;
-    esac
+  case $opt in
+    r ) rm -rf ~/.oh-my-zsh ~/.hushlogin ~/.zshrc ~/.termux; pkg remove zsh neofetch; exit;;
+    \? ) echo "Usage: runme.sh [-r]"; exit;;
+  esac
 done
 dir=$(cd "$(dirname "$0")";pwd)
 cp -r $dir/.termux ~
@@ -12,15 +12,22 @@ pkg install zsh neofetch
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s zsh
 while true; do
-    read -p "Powerlevel9k or Powerlevel10k? [A/B]" input
-    case $input in
-        [Aa] ) git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k; break;;
-        [Bb] ) git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k; break;;
-        * ) echo "Invalid input... Try again.";;
-    esac
+  read -p "Powerlevel9k or Powerlevel10k? [A/B] " input
+  case $input in
+    [Aa] ) git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k; break;;
+    [Bb] ) git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k; break;;
+    * ) echo "Invalid input... Try again.";;
+  esac
 done
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 cp $dir/.zshrc ~
+if $input == [Aa]
+then
+  echo "ZSH_THEME=\"powerlevel9k/powerlevel9k\"" >> ~/.zshrc
+else
+  echo "ZSH_THEME=\"powerlevel10k/powerlevel10k\"" >> ~/.zshrc
+fi
+echo "neofetch" >> ~/.zshrc
 echo "Done."
