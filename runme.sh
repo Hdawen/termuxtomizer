defd841e494d7b14d9c1f13403af265cc4730476
mechtifs <mechtifs@gmail.com>
2019-08-19 01:52:12 +0800

    Optimized script.

--- runme.sh
+++ runme.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cp -r .termux ~
-cp .hushlogin ~
+cp -r $(shell pwd)/.termux ~
+cp $(shell pwd)/.hushlogin ~
 pkg install zsh wget neofetch
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 chsh -s zsh
@@ -8,4 +8,4 @@ git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/theme
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-cp .zshrc ~
+cp $(shell pwd)/.zshrc ~
