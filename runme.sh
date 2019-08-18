ba8701318fa60ff41eee88205e0ccc89255e8446
mechtifs <mechtifs@gmail.com>
2019-08-19 02:56:28 +0800

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
-
 while getopts ":r" opt; do
     case $opt in
-        r ) rm -rf ~/oh-my-zsh ~/.hushlogin ~/.zshrc ~/.termux; pkg remove zsh neofetch; exit;;
+        r ) rm -rf ~/.oh-my-zsh ~/.hushlogin ~/.zshrc ~/.termux; pkg remove zsh neofetch; exit;;
         \? ) echo "Usage: runme.sh [-r]"; exit;;
     esac
 done
-
 dir=$(cd "$(dirname "$0")";pwd)
 cp -r $dir/.termux ~
 cp $dir/.hushlogin ~
 pkg install zsh neofetch
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s zsh
-
 while true; do
     read -p "Powerlevel9k or Powerlevel10k? [A/B]" input
     case $input in
@@ -22,7 +19,6 @@ while true; do
         * ) echo "Invalid input... Try again.";;
     esac
 done
-
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
