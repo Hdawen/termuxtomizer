847ef76f16f7ebbd9015440ba63c263a844c6190
mechtifs <mechtifs@gmail.com>
2019-08-19 02:48:43 +0800

    Updated runme.sh

--- runme.sh
+++ runme.sh
@@ -1,11 +1,30 @@
 #!/bin/bash
-cp -r $(pwd)/.termux ~
-cp $(pwd)/.hushlogin ~
-pkg install zsh wget neofetch
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+while getopts ":r" opt; do
+    case $opt in
+        r ) rm -rf ~/oh-my-zsh ~/.hushlogin ~/.zshrc ~/.termux; pkg remove zsh neofetch;;
+        \? ) echo "Usage: runme.sh [-r]"
+    esac
+done
+
+dir=$(cd "$(dirname "$0")";pwd)
+cp -r $dir/.termux ~
+cp $dir/.hushlogin ~
+pkg install zsh neofetch
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s zsh
-git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k
+
+while true; do
+    read -p "Powerlevel9k or Powerlevel10k? [A/B]" input
+    case $input in
+        [Aa] ) git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k; break;;
+        [Bb] ) git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k; break;;
+        * ) echo "Invalid input... Try again.";;
+    esac
+done
+
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-cp $(pwd)/.zshrc ~
+cp $dir/.zshrc ~
+echo "Done."
