6af7dd368e2d514b1672cf264902e74381863a1e
mechtifs <mechtifs@gmail.com>
2019-08-19 03:41:26 +0800

    Roll back to powerlevel9k, neofetch is now optional

--- runme.sh
+++ runme.sh
@@ -8,26 +8,20 @@ done
 dir=$(cd "$(dirname "$0")";pwd)
 cp -r $dir/.termux ~
 cp $dir/.hushlogin ~
-pkg install zsh neofetch
+pkg install zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 chsh -s zsh
-while true; do
-  read -p "Powerlevel9k or Powerlevel10k? [A/B] " input
-  case $input in
-    [Aa] ) git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k; break;;
-    [Bb] ) git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/custom/themes/powerlevel10k; break;;
-    * ) echo "Invalid input... Try again.";;
-  esac
-done
+git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/custom/plugins/zsh-completions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 cp $dir/.zshrc ~
-if [ $input = [Aa] ]
-then
-  echo "ZSH_THEME=\"powerlevel9k/powerlevel9k\"" >> ~/.zshrc
-else
-  echo "ZSH_THEME=\"powerlevel10k/powerlevel10k\"" >> ~/.zshrc
-fi
-echo "neofetch" >> ~/.zshrc
-echo "Done."
+while true; do
+  read -p "Would you like to have neofetch on startup? [Y/n] " input
+  case $input in
+    [Yy] ) pkg install neofetch; echo "neofetch" >> ~/.zshrc; break;;
+    [Nn] ) break;;
+    * ) echo "Invalid input... Try again.";;
+  esac
+done
+echo "Done. Restart session to take effects."
